1a7b3704d09c935514e17233687331e56db0757b
web3worker <140084035+web3woker0001@users.noreply.github.com>
2024-06-14 18:06:39 +0800

    Update ionet_install.sh

--- ionet_install.sh
+++ ionet_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-mark hold linux-image-generic linux-headers-generic
+# sudo apt-mark hold linux-image-generic linux-headers-generic
 # 下载安装脚本
 echo "下载 Ionet 安装脚本..."
 curl -L https://github.com/ionet-official/io-net-official-setup-script/raw/main/ionet-setup.sh -o ionet-setup.sh
